3d822cf8183c48e99191cb6fd4727212701866f6
Lu-dashuai <aluruihua@163.com>
2020-05-28 15:28:33 +0800

    '数组实现环形队列'

--- src/jack/queue/CircleQueue.java
+++ src/jack/queue/CircleQueue.java
@@ -1,5 +1,7 @@
 package jack.queue;
 
+import java.util.LinkedList;
+
 /**
  * 一些声明信息
  * Description: 数组实现环形队列<br/>
@@ -10,7 +12,19 @@ package jack.queue;
  */
 public class CircleQueue {
     public static void main(String[] args) {
-
+        LinkedQueue linkedQueue = new LinkedQueue(5);
+        linkedQueue.add(10);
+        linkedQueue.add(20);
+        linkedQueue.add(30);
+        linkedQueue.add(40);
+        linkedQueue.add(50);
+        linkedQueue.show();
+        int size = linkedQueue.getSize();
+        System.out.println("长度："+size);
+        linkedQueue.del();
+        linkedQueue.show();
+        size = linkedQueue.getSize();
+        System.out.println("长度："+size);
     }
 
 }
@@ -56,9 +70,9 @@ class LinkedQueue {
             System.out.println("队列已满~~");
             return;
         }
-        this.food = (this.food % size) + 1; //food向后移一位
-        this.arr[this.food] = val;//给值
-        System.out.println("添加成功,food为：" + this.food);
+        arr[food] = val;//给值
+        food = (food + 1 ) % size; //food向后移一位
+        System.out.println("添加成功,food为：" + food);
     }
 
     /**
@@ -69,9 +83,9 @@ class LinkedQueue {
             System.out.println("队列为null~~~");
             return;
         }
-        this.arr[this.head] = 0;
-        this.head = (this.head % size) + 1;
-        System.out.println("添加成功,head为：" + this.head);
+        int val = arr[head];
+        this.head = ( head + 1) % size;
+        System.out.println("删除["+val+"]成功,head为：" + head);
     }
 
     /**
@@ -80,14 +94,18 @@ class LinkedQueue {
      * @return
      */
     public int getSize() {
-        if (isFull()) {
-            return this.size;
-        }
-        if (isNull()) {
-            return 0;
-        }
-        return Math.abs(this.head - this.food);
+        return  (food - head + size) % size;
     }
 
+    /**
+     * 展示当前队列
+     */
+    public void show(){
+        //从head开始，表示循环有效长度次
+        for (int i = head; i < head + getSize(); i++) {
+            //取余等于拿到真实的 数组内的指针
+            System.out.println("arr["+i%size+"] = "+arr[i%size]);
+        }
+    }
 
 }
